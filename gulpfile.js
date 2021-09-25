07f65bed447cebfdd92d621c4c5d67e90ef87a6d
senspond20 <senspond@gmail.com>
2021-09-26 05:04:36 +0900

    gulp-uglify

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,11 @@
 import gulp from 'gulp'
+import uglify from 'gulp-uglify'
 // import babel from 'gulp-babel'
 
 console.log('start')
 gulp.task('default', () =>
     gulp.src('src/**/*.js')
+        .pipe(uglify())
         // .pipe(babel())
         .pipe(gulp.dest('dist'))
 );
